cdf62720db39d231465de46a60425aa0a3dc9d4d
Eric Zacharia <ekzacharia@gmail.com>
2022-04-14 16:05:48 -0500

    include api credentials example file

---
+++ EXAMPLE_api_credentials.py
@@ -0,0 +1,8 @@
+TWITTER_ACCESS_TOKEN = "########-##############"
+TWITTER_ACCESS_TOKEN_SECRET = "##########"
+TWITTER_CONSUMER_KEY = "##########"
+TWITTER_CONSUMER_SECRET = "##########"
+
+ALPACA_ENDPOINT_URL = "https://paper-api.alpaca.markets"
+ALPACA_API_KEY = "##########"
+ALPACA_SECRET_KEY = "##########"
